37305b7d5b7f5e11cc6a708901877da283668c4e
cin5mada <cin5marian@gmail.com>
2021-05-13 21:30:44 +0300

    first commit

---
+++ contacts.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# contacts_project/contacts.py
+
+"""This module provides Contacts entry point script."""
+
+from contacts.main import main
+
+if __name__ == "__main__":
+    main()
